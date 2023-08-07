7efec3c8e0ec1ac0a063f2cd8b614404dac0eb29
JYROH <ghgt1@naver.com>
2023-08-08 02:00:48 +0900

    feat: 방 생성 페이지 제작 및 주소 복사 기능 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as RectangleBtn } from './RectangleBtn/RectangleBtn';
 export { default as AnimateLayout } from './AnimateLayout/AnimateLayout';
+export { default as Tooltip } from './Tooltip/Tooltip';
--- src/pages/CreateRoom/CreateRoom.tsx
+++ src/pages/CreateRoom/CreateRoom.tsx
@@ -1,18 +1,36 @@
+import { useState } from 'react';
 import { motion } from 'framer-motion';
+import { CopyText, StyledContainer, StyledHeader, TestContainer, UrlText } from './styles';
+import { RectangleBtn, Tooltip } from '@/components';
+import { clipboardCopy } from '@/utils/clipboardCopy';
 
 export default function CreateRoom() {
+  const [url] = useState('http://awefas.fasefasdf.aewf');
+  const [toolTip, setTooltip] = useState(false);
+
+  const handleCopyURL = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    clipboardCopy(url)(e);
+    setTooltip(true);
+    setTimeout(() => {
+      setTooltip(false);
+    }, 2000);
+  };
+
   return (
     <motion.div
-      key="main"
       initial={{ opacity: 0, x: 100 }}
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: -100 }}
     >
-      <div>
-        <p>애니메이션 테스트</p>
-        <p>애니메이션 테스트</p> <p>애니메이션 테스트</p> <p>애니메이션 테스트</p>{' '}
-        <p>애니메이션 테스트</p> <p>애니메이션 테스트</p>
-      </div>
+      <StyledContainer>
+        <StyledHeader>우리 팀의 방이 생성되었어요</StyledHeader>
+        <TestContainer />
+        {/* TODO: 테스트용 URL. 추후에 api와 결합하여 URL작성 */}
+        <UrlText>{url}</UrlText>
+        <CopyText onClick={handleCopyURL}>복사해서 팀원에게 공유하기</CopyText>
+        {toolTip && <Tooltip>URL이 클립보드에 복사되었습니다</Tooltip>}
+        <RectangleBtn text="시작하기" />
+      </StyledContainer>
     </motion.div>
   );
 }
