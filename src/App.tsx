8174eed691f5f09783ee436cf18d56a55d2f02c6
JYROH <ghgt1@naver.com>
2023-08-08 16:31:03 +0900

    feat: QueryClient로 APP서비스 감싸고 Dev tool 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { GlobalStyle } from './GlobalStyle';
 import router from './Router';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <>
-      <GlobalStyle />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </>
   );
 }
