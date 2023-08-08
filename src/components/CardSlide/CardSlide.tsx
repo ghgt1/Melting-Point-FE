006e8c99fb8fbc36dfa2c80520d80d7dacb818eb
JYROH <ghgt1@naver.com>
2023-08-09 04:46:16 +0900

    feat: 카드 슬라이드 컴포넌트 생성

---
+++ src/components/CardSlide/CardSlide.tsx
@@ -0,0 +1,46 @@
+import { CardContainer, ListContainer, Marker, BigMarker, QuestionContainer } from './styles';
+import { useState } from 'react';
+import { useTestContext } from '@/hooks/useTestContext';
+
+type CardSlideProps = { question: string; alpha: string; beta: string };
+
+export default function CardSlide({ question, alpha, beta }: CardSlideProps) {
+  const [select, setSelect] = useState<number>();
+
+  const { setResult } = useTestContext();
+
+  const handleClickTestFirst = () => {
+    setSelect(1);
+    if (!select) {
+      setResult((prevState: string) => prevState + '1');
+    } else {
+      setResult((prevState: string) => prevState.slice(0, -1) + '1');
+    }
+  };
+
+  const handleClickTestSecond = () => {
+    setSelect(2);
+    if (!select) {
+      setResult((prevState: string) => prevState + '2');
+    } else {
+      setResult((prevState: string) => prevState.slice(0, -1) + '2');
+    }
+  };
+
+  return (
+    <CardContainer>
+      <QuestionContainer>
+        <BigMarker>Q.</BigMarker>
+        {question}
+      </QuestionContainer>
+      <ListContainer check={select === 1} onClick={handleClickTestFirst}>
+        <Marker>A.</Marker>
+        {alpha}
+      </ListContainer>
+      <ListContainer check={select === 2} onClick={handleClickTestSecond}>
+        <Marker>B.</Marker>
+        {beta}
+      </ListContainer>
+    </CardContainer>
+  );
+}
