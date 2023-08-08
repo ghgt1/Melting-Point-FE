d0f1d4cc81f1e9a3d47d3095002eff7bb71d5db0
JYROH <ghgt1@naver.com>
2023-08-09 04:46:36 +0900

    feat: 다음 페이지 이동 ui 컴포넌트 생성

---
+++ src/components/NextBtn/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const MainButton = styled.button`
+  background-color: #1a1e27;
+  font-size: 20px;
+  font-weight: bold;
+  color: #ffffff;
+  width: 375px;
+  height: 90px;
+  border-radius: 12px 12px 0 0;
+  cursor: pointer;
+`;
