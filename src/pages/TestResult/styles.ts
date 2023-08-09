1b231f646b5801cd368f52407b2a4e6251c5af61
JYROH <ghgt1@naver.com>
2023-08-09 13:41:06 +0900

    feat: 테스트 결과페이지 생성 및 작업

---
+++ src/pages/TestResult/styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  min-height: 100vh;
+`;
+
+export const DoneText = styled.p`
+  color: #838383;
+  font-size: 16px;
+  margin-top: 30px;
+  height: 22px;
+`;
+
+export const TitleText = styled.p`
+  font-size: 22px;
+  font-weight: bold;
+  color: #ffffff;
+  margin-top: 4px;
+  height: 60px;
+  white-space: pre-line;
+  text-align: center;
+`;
+
+export const CardImage = styled.img`
+  width: 280px;
+  height: 400px;
+  margin-top: 35px;
+  margin-bottom: 30px;
+`;
+
+export const ExplainText = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+  color: #ffffff;
+  height: 26px;
+  margin-top: 4px;
+`;
+
+export const NickNameText = styled.p`
+  height: 30px;
+  font-size: 22px;
+  font-weight: bold;
+  color: #ffffff;
+  text-align: center;
+`;
