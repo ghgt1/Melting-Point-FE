9fee3d4e64d093ae2bb6cc92e99a101e7653bf8e
JYROH <ghgt1@naver.com>
2023-08-10 17:04:08 +0900

    feat: 쿼리키에 방생성, 방조회 api 키 추가

--- src/constants/querykey.ts
+++ src/constants/querykey.ts
@@ -1,2 +1,8 @@
 // URL 생성
 export const CREATE_ROOM_URL_KEY = 'create-room-url';
+
+// URL 대기방 인원 확인
+export const GET_ROOM_STATUS_KEY = 'get-room-status';
+
+// URL에 유저 진입후 가입
+export const POST_ROOM_CHECKIN_KEY = 'post-room-checkin';
