a56c4d72297674012d42ff2fc03757f7695e3f8b
JYROH <ghgt1@naver.com>
2023-08-10 23:48:50 +0900

    feat: 게임 설명 페이지 생성

---
+++ src/pages/GameExplain/GameExplain.tsx
@@ -0,0 +1,33 @@
+import { StyledContainer, StyledDescription, StyledTitle, ExplainImg } from './styles';
+import gameExplain from '@assets/gameExplain.png';
+import { NextBtn } from '@/components';
+import { useNavigate, useParams } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+export default function GameExplain() {
+  const { token } = useParams();
+  const navigate = useNavigate();
+
+  const handleNextPage = () => {
+    navigate(`/game/lobby/${token}`);
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: 100 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -100 }}
+    >
+      <StyledContainer>
+        <StyledTitle>
+          우리 팀의 아이스브레이킹은
+          <br />
+          이렇게 진행될 예정이에요
+        </StyledTitle>
+        <StyledDescription>아직은 차가운 분위기를 녹여볼까요?</StyledDescription>
+        <ExplainImg src={gameExplain} alt="게임 설명 이미지" />
+        <NextBtn text="시작할래요!" onClick={handleNextPage} />
+      </StyledContainer>
+    </motion.div>
+  );
+}
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,3 +4,4 @@ export { default as TestExplain } from './TestExplain/TestExplain';
 export { default as TestPage } from './TestPage/TestPage';
 export { default as TestResult } from './TestResult/TestResult';
 export { default as GameLobby } from './GameLobby/GameLobby';
+export { default as GameExplain } from './GameExplain/GameExplain';
