1098bef0b7fcd19206ed7c7d654a78e7f05c32e6
JYROH <ghgt1@naver.com>
2023-08-11 02:41:02 +0900

    feat: 형용사 목록 배열 상수화

---
+++ src/constants/adjectiveList.ts
@@ -0,0 +1,66 @@
+export const adjectiveList = [
+  {
+    id: 1,
+    expression: '꼼꼼한',
+  },
+  {
+    id: 2,
+    expression: '솔직한',
+  },
+  {
+    id: 3,
+    expression: '자신감 있는',
+  },
+  {
+    id: 4,
+    expression: '사려깊은',
+  },
+  {
+    id: 5,
+    expression: '신중한',
+  },
+  {
+    id: 6,
+    expression: '쾌활한',
+  },
+  {
+    id: 7,
+    expression: '침착한',
+  },
+  {
+    id: 8,
+    expression: '내성적인',
+  },
+  {
+    id: 9,
+    expression: '외향적인',
+  },
+  {
+    id: 10,
+    expression: '긍정적인',
+  },
+  {
+    id: 11,
+    expression: '열정적인',
+  },
+  {
+    id: 12,
+    expression: '다정한',
+  },
+  {
+    id: 13,
+    expression: '부지런한',
+  },
+  {
+    id: 14,
+    expression: '정직한',
+  },
+  {
+    id: 15,
+    expression: '즉흥적인',
+  },
+  {
+    id: 16,
+    expression: '엉뚱한',
+  },
+];
