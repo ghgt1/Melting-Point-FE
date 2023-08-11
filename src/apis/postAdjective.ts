e44fbebd33c6e92b62a201953a1518c6958184d0
JYROH <ghgt1@naver.com>
2023-08-11 16:05:53 +0900

    feat: Navigate 로직 event sse로 이동

--- src/apis/postAdjective.ts
+++ src/apis/postAdjective.ts
@@ -1,7 +1,6 @@
 import instance from './axios';
 import { useMutation } from '@tanstack/react-query';
 import { POST_ADJECTIVE_USER_KEY } from '@/constants/querykey';
-import { useNavigate } from 'react-router-dom';
 
 type postAdjectiveParams = {
   url: string;
@@ -20,13 +19,9 @@ export const postAdjective = async (params: postAdjectiveParams) => {
 };
 
 export const useAdjective = (params: postAdjectiveParams) => {
-  const navigate = useNavigate();
   return useMutation([POST_ADJECTIVE_USER_KEY], () => postAdjective(params), {
     onError: (error) => {
       console.error('Query 처리중 에러가 발생하였습니다:', error);
-    },
-    onSuccess: () => {
-      navigate(`/game/${params.url}`);
     },
   });
 };
