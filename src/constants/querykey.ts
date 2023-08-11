8515e2623351f8ceadcdea6cf1de28b20161cb60
JYROH <ghgt1@naver.com>
2023-08-11 16:07:18 +0900

    feat: 쿼리키에 형용사 목록 get 쿼리키 추가

--- src/constants/querykey.ts
+++ src/constants/querykey.ts
@@ -12,3 +12,6 @@ export const PATCH_ROOM_READY_KEY = 'patch-room-ready';
 
 // 형용사 선택한것 서버로 전송
 export const POST_ADJECTIVE_USER_KEY = 'post-adjective-user';
+
+// 이용자들 형용사 선택 목록 받기
+export const GET_USERS_ADJECTIVE_KEY = 'get-users-adjective';
