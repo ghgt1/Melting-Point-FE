c77249ce80f720e2e7ef9d952a280fe5016e8744
JYROH <ghgt1@naver.com>
2023-08-11 16:15:44 +0900

    feat: 형용사 페이지 로딩 및 방어로직 추가 및 SSE 적요

--- src/pages/Adjective/Adjective.tsx
+++ src/pages/Adjective/Adjective.tsx
@@ -7,8 +7,11 @@ import {
   AdjectiveBox,
 } from './styles';
 import { adjectiveList } from '@/constants/adjectiveList';
-import { NextBtn, Tooltip } from '@/components';
+import { NextBtn, Tooltip, LoadingOverlay } from '@/components';
 import { useTooltip } from '@/hooks/useTooltip';
+import { motion } from 'framer-motion';
+import { useAdjective } from '@/apis/postAdjective';
+import { useParams, useNavigate } from 'react-router-dom';
 
 type AdjectiveDTO = {
   id: number;
@@ -18,10 +21,19 @@ type AdjectiveDTO = {
 export default function Adjective() {
   const [adjectives, setAdjectives] = useState<number[]>([]);
   const [isDone, setIsDone] = useState(false);
+  const [clicked, setClicked] = useState(false);
   const { toolTip, setTooltipVisible } = useTooltip();
+  const { token, userId } = useParams();
+  const navigate = useNavigate();
+  const { mutate } = useAdjective({
+    url: token || ' ',
+    user_id: Number(userId) || 0,
+    expression_id: adjectives,
+  });
 
   const handleNextPage = () => {
-    console.log('ZC');
+    setClicked(true);
+    mutate();
   };
 
   const handleAdjectiveClick = (id: number) => {
@@ -45,35 +57,59 @@ export default function Adjective() {
     }
   }, [adjectives]);
 
+  useEffect(() => {
+    const eventSource = new EventSource(`${import.meta.env.VITE_API_URL}/events`);
+    eventSource.onmessage = (event) => {
+      // url검증
+      const data = JSON.parse(event.data);
+      if (data.url === token) {
+        if (data.status) {
+          navigate(`/game/adjective/result/${userId}/${token}`);
+        }
+      }
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
   return (
-    <StyledContainer>
-      <StyledTitle>
-        나를 표현할 수 있는
-        <br />
-        형용사를 선택해주세요
-      </StyledTitle>
-      <StyledDescription>
-        최소 1가지이상을 선택해주세요
-        <br />
-        최대 4가지까지 선택이 가능해요
-      </StyledDescription>
-      <AdjectiveContainer>
-        {adjectiveList.map((adjective: AdjectiveDTO, index: number) => {
-          return (
-            <AdjectiveBox
-              key={adjective.id}
-              $isChecked={adjectives.includes(index + 1)}
-              onClick={() => {
-                handleAdjectiveClick(adjective.id);
-              }}
-            >
-              {adjective.expression}
-            </AdjectiveBox>
-          );
-        })}
-      </AdjectiveContainer>
-      <NextBtn text="결과 볼게요!" onClick={handleNextPage} isFinish={isDone} />
-      {toolTip && <Tooltip>형용사는 최소 1개에서 최대 4개까지 선택 가능해요</Tooltip>}
-    </StyledContainer>
+    <motion.div
+      initial={{ opacity: 0, x: 100 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -100 }}
+    >
+      <StyledContainer>
+        <StyledTitle>
+          나를 표현할 수 있는
+          <br />
+          형용사를 선택해주세요
+        </StyledTitle>
+        <StyledDescription>
+          최소 1가지이상을 선택해주세요
+          <br />
+          최대 4가지까지 선택이 가능해요
+        </StyledDescription>
+        <AdjectiveContainer>
+          {adjectiveList.map((adjective: AdjectiveDTO, index: number) => {
+            return (
+              <AdjectiveBox
+                key={adjective.id}
+                $isChecked={adjectives.includes(index + 1)}
+                onClick={() => {
+                  handleAdjectiveClick(adjective.id);
+                }}
+              >
+                {adjective.expression}
+              </AdjectiveBox>
+            );
+          })}
+        </AdjectiveContainer>
+        <NextBtn text="결과 볼게요!" onClick={handleNextPage} isFinish={isDone} />
+        {toolTip && <Tooltip>형용사는 최소 1개에서 최대 4개까지 선택 가능해요</Tooltip>}
+        {clicked && <LoadingOverlay />}
+      </StyledContainer>
+    </motion.div>
   );
 }
