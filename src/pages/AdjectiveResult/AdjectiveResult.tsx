b9f6433f928fdeab1bb973f5c8657b506b3696ac
JYROH <ghgt1@naver.com>
2023-08-11 17:29:41 +0900

    feat: 형용사 결과 페이지에서 공통질문 페이지 이동로직 작성

--- src/pages/AdjectiveResult/AdjectiveResult.tsx
+++ src/pages/AdjectiveResult/AdjectiveResult.tsx
@@ -10,47 +10,81 @@ import {
 } from './styles';
 import charWithHat from '@assets/charWithHat.png';
 import { useAdjectiveList } from '@/apis/getAdjectiveList';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { returnChar } from '@/utils/returnChar';
 import { AdjectiveListDto } from '@/types/AdjectiveListDto';
-import { CharBlock, NextBtn } from '@/components';
+import { CharBlock, NextBtn, MeltShow } from '@/components';
+import { useNextGame } from '@/apis/postNextGame';
+import { useEffect, useState } from 'react';
 
 export default function AdjectiveResult() {
-  const { token } = useParams();
+  const { token, userId } = useParams();
   const { data } = useAdjectiveList(token || ' ');
 
+  const { mutate } = useNextGame(token || ' ');
+
+  const navigate = useNavigate();
+
+  const [meltShow, setMeltShow] = useState(false);
+
   const handleNextPage = () => {
-    console.log('다음페이지이동!');
+    mutate();
   };
 
+  useEffect(() => {
+    const eventSource = new EventSource(`${import.meta.env.VITE_API_URL}/events`);
+    eventSource.onmessage = (event) => {
+      // url검증
+      const data = JSON.parse(event.data);
+      if (data.url === token) {
+        if (data.status) {
+          setMeltShow(true);
+          setTimeout(() => {
+            navigate(`/game/question/${userId}/${token}`);
+          }, 3000);
+        }
+      }
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
   return (
-    <StyledContainer>
-      <TopContainer>
-        <StyledTitle>
-          형용사를 통해서 <br /> 나를 소개해보세요!
-        </StyledTitle>
-        <CharImg src={charWithHat} alt="마스코트 이미지" />
-      </TopContainer>
-      <TotalContainer>
-        {data?.map((user: AdjectiveListDto) => {
-          return (
-            <ResultContainer>
-              <CharBlock
-                text={user.nickname}
-                imgSrc={returnChar(user.img_id)}
-                imgWidth={'80px'}
-                textSize={'14px'}
-              />
-              <GridContainer>
-                {user.expressions.map((expression) => {
-                  return <AdjectiveBlock>{expression}</AdjectiveBlock>;
-                })}
-              </GridContainer>
-            </ResultContainer>
-          );
-        })}
-      </TotalContainer>
-      <NextBtn text="다음 게임 할래요!" onClick={handleNextPage} />
-    </StyledContainer>
+    <>
+      {meltShow ? (
+        <MeltShow id={1} />
+      ) : (
+        <StyledContainer>
+          <TopContainer>
+            <StyledTitle>
+              형용사를 통해서 <br /> 나를 소개해보세요!
+            </StyledTitle>
+            <CharImg src={charWithHat} alt="마스코트 이미지" />
+          </TopContainer>
+          <TotalContainer>
+            {data?.map((user: AdjectiveListDto) => {
+              return (
+                <ResultContainer key={user.nickname}>
+                  <CharBlock
+                    text={user.nickname}
+                    imgSrc={returnChar(user.img_id)}
+                    imgWidth={'80px'}
+                    textSize={'14px'}
+                  />
+                  <GridContainer>
+                    {user.expressions.map((expression) => {
+                      return <AdjectiveBlock>{expression}</AdjectiveBlock>;
+                    })}
+                  </GridContainer>
+                </ResultContainer>
+              );
+            })}
+          </TotalContainer>
+          <NextBtn text="다음 게임 할래요!" onClick={handleNextPage} />
+        </StyledContainer>
+      )}
+    </>
   );
 }
