f4303e50bc7baa6e030db2cb87d89f225ec6c1d6
JYROH <ghgt1@naver.com>
2023-08-12 18:03:04 +0900

    feat: 로딩 오버레이에 스피너만 있는 경우 생성

--- src/components/LoadingOverlay/LoadingOverlay.tsx
+++ src/components/LoadingOverlay/LoadingOverlay.tsx
@@ -1,12 +1,18 @@
 import { LoadingContainer, LoadingSpinner, LoadingExplain } from './styles';
 
-export default function LoadingOverlay() {
+type LoadingOverlayProps = {
+  onlySpinner?: boolean;
+};
+
+export default function LoadingOverlay({ onlySpinner = false }: LoadingOverlayProps) {
   return (
     <LoadingContainer>
-      <LoadingExplain>
-        다른 팀원이 완료될 때 까지 <br />
-        기다리고 있어요!
-      </LoadingExplain>
+      {!onlySpinner && (
+        <LoadingExplain>
+          다른 팀원이 완료될 때 까지 <br />
+          기다리고 있어요!
+        </LoadingExplain>
+      )}
       <LoadingSpinner />
     </LoadingContainer>
   );
