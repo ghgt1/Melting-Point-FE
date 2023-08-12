0686f78efc4b355db649d51eb24b2ba7a5993157
JYROH <ghgt1@naver.com>
2023-08-12 21:56:56 +0900

    fix: 질문이미지 수정 및 직접 렌더링 방식으로 변경

--- src/pages/Question/Question.tsx
+++ src/pages/Question/Question.tsx
@@ -4,7 +4,16 @@ import question1 from '@assets/question1.png';
 import question2 from '@assets/question2.png';
 import question3 from '@assets/question3.png';
 import question4 from '@assets/question4.png';
-import { StyledContainer, TopContainer, StyledTitle, CharImg, QuestionImg } from './styles';
+import {
+  StyledContainer,
+  TopContainer,
+  StyledTitle,
+  CharImg,
+  QuestionContainer,
+  PageNotice,
+  StyledDescription,
+  QuestionImg,
+} from './styles';
 import { pageCountConvert } from '@/utils/pageCountConvert';
 import { AnimatePresence, motion } from 'framer-motion';
 import { NextBtn, MeltShow } from '@/components';
@@ -79,7 +88,14 @@ export default function Question() {
                       animate={{ opacity: 1, x: 0 }}
                       exit={{ opacity: 0, x: -100 }}
                     >
-                      <QuestionImg src={question1} alt="1번질문지" />
+                      <QuestionContainer>
+                        <PageNotice>1/4</PageNotice>
+                        <StyledDescription>
+                          가장 최근에 본<br />
+                          영화는 무엇인가요?
+                        </StyledDescription>
+                        <QuestionImg src={question1} alt="질문 이미지" />
+                      </QuestionContainer>
                     </motion.div>
                   )}
                   {page === 2 && (
@@ -88,7 +104,17 @@ export default function Question() {
                       animate={{ opacity: 1, x: 0 }}
                       exit={{ opacity: 0, x: -100 }}
                     >
-                      <QuestionImg src={question2} alt="2번질문지" />
+                      <QuestionContainer>
+                        <PageNotice>2/4</PageNotice>
+                        <StyledDescription>
+                          일하면서 음악을 즐겨듣나요?
+                          <br />
+                          듣는다면 어떤 종류의
+                          <br />
+                          음악을 듣나요?
+                        </StyledDescription>
+                        <QuestionImg src={question2} alt="질문 이미지" />
+                      </QuestionContainer>
                     </motion.div>
                   )}
                   {page === 3 && (
@@ -97,7 +123,15 @@ export default function Question() {
                       animate={{ opacity: 1, x: 0 }}
                       exit={{ opacity: 0, x: -100 }}
                     >
-                      <QuestionImg src={question3} alt="3번질문지" />
+                      <QuestionContainer>
+                        <PageNotice>3/4</PageNotice>
+                        <StyledDescription>
+                          현재 노트북 배경화면은
+                          <br />
+                          무엇인가요?
+                        </StyledDescription>
+                        <QuestionImg src={question3} alt="질문 이미지" />
+                      </QuestionContainer>
                     </motion.div>
                   )}
                   {page === 4 && (
@@ -106,7 +140,15 @@ export default function Question() {
                       animate={{ opacity: 1, x: 0 }}
                       exit={{ opacity: 0, x: -100 }}
                     >
-                      <QuestionImg src={question4} alt="4번질문지" />
+                      <QuestionContainer>
+                        <PageNotice>4/4</PageNotice>
+                        <StyledDescription>
+                          내일 세상이 멸망한다면
+                          <br />
+                          무엇을 하고싶나요?
+                        </StyledDescription>
+                        <QuestionImg src={question4} alt="질문 이미지" />
+                      </QuestionContainer>
                     </motion.div>
                   )}
                 </>
--- src/pages/Question/styles.ts
+++ src/pages/Question/styles.ts
@@ -29,8 +29,46 @@ export const CharImg = styled.img`
   width: 87px;
 `;
 
-export const QuestionImg = styled.img`
+export const QuestionContainer = styled.div`
   margin-top: 28px;
+  background: linear-gradient(152.16deg, #002b78, #050d1e);
+  border-radius: 15px;
   width: 327px;
   height: 445px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 30px;
+`;
+
+export const PageNotice = styled.div`
+  margin-top: 46px;
+  width: 95px;
+  height: 30px;
+  border-radius: 15px;
+  padding: 10px;
+  font-size: 18px;
+  font-weight: 600;
+  background-color: #3163cf;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledDescription = styled.p`
+  font-weight: 600;
+  line-height: 34px;
+  font-size: 22px;
+  white-space: pre-line;
+  margin-top: 20px;
+  text-align: center;
+  height: 102px;
+  display: flex;
+  align-items: center;
+`;
+
+export const QuestionImg = styled.img`
+  margin-top: 35px;
+  width: 230px;
+  height: 188px;
 `;
